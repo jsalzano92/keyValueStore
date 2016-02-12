5aafd0eed46b1644eae6c3aa87d7fbb3a72e77ac
jsalzano92 <jsalzano@rams.colostate.edu>
2016-02-12 16:33:46 -0700

    Prints a string after given command to do so.

--- keyValueStore.c
+++ keyValueStore.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+char string[80];
 
 int main(int argc, char* argv[]){
-	if(argc == 2)
-		printf("Hello, %s\n", argv[1]);
-	else
-		printf("Hello, world!\n");
+	printf("Enter a string: ");
+	scanf("%s", &string);
+	
+	char command[10];
+	do {
+		printf("Command: ");
+		scanf("%s", &command);
+	} while(strcmp(command, "print") != 0);
+	
+	printf("%s\n", string);
 	
 	return 0;
 }
